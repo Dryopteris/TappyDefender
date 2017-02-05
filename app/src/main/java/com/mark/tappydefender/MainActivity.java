4664db79db692c8b3392d9f4262c3c16baf2819a
Mark Bann <markbann44@gmail.com>
2017-02-05 13:28:19 -0500

    Initial commit of Tappy Defender following Packt

---
+++ app/src/main/java/com/mark/tappydefender/MainActivity.java
@@ -0,0 +1,29 @@
+package com.mark.tappydefender;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity
+        implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        // Starts GameActivity
+        startActivity(intent);
+        // Shuts MainActivity down
+        finish();
+    }
+}
